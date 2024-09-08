8a3dbf23aae6bac0c4ac6455ad9963fd30fb8387
ashrafulatif <ashrafulhaque08@gmail.com>
2024-09-09 02:14:04 +0600

    Auth[BDF]

---
+++ WebAppCRUDEFDBF/WebAppCRUDEF/Controllers/EmployeeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCRUDEF.Context;
+
+namespace WebAppCRUDEF.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        AspDBEntitiesContext _dbContext;
+
+        public EmployeeController()
+        {
+            _dbContext = new AspDBEntitiesContext();
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            //session check 
+            if (Session["username"] == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            var empList = _dbContext.Employees.ToList();
+            ViewBag.Success = "Login Successful !!!";
+
+            return View(empList);
+        }
+
+        
+        public ActionResult AddEmployee()
+        {
+            //session check 
+            if (Session["username"] == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            if (Session["type"].ToString().Equals("Member"))
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CerateEmployee(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Employees.Add(emp);
+                _dbContext.SaveChanges();
+                TempData["msgAdd"] = "Employee Information added sucessfully!";
+
+                return RedirectToAction("Index");
+            }
+            
+            return RedirectToAction("AddEmployee");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var emp = _dbContext.Employees.Where(x=>x.Id==id).FirstOrDefault();
+            _dbContext.Employees.Remove(emp);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+} 
---
+++ WebAppCRUDEFDBF/WebAppCRUDEF/Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCRUDEF.Context;
+
+namespace WebAppCRUDEF.Controllers
+{
+    public class UserController : Controller
+    {
+        AspDBEntitiesContext _dbContext;
+
+        public UserController()
+        {
+            _dbContext = new AspDBEntitiesContext();
+        }
+
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            ModelState.Remove("RePassword");
+
+            if (ModelState.IsValid)
+            {
+                var data =_dbContext.Users.Where(x=>x.Username == u.Username && x.Password == u.Password).FirstOrDefault();
+
+                if (data != null)
+                {
+                    Session["username"] = data.Username;
+                    Session["type"] = data.Type;
+
+                    return RedirectToAction("Index", "Employee");
+                }
+
+                ViewBag.Invalid="Invalid User";
+                ModelState.Clear();
+                return View();
+            }
+
+            ModelState.Clear();
+            return View();
+           
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()  
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User u)
+        {
+
+            if (_dbContext.Users.Any(x => x.Username == u.Username))
+            {
+                ModelState.Clear();  
+                return View();
+            }
+
+            _dbContext.Users.Add(u);
+            _dbContext.SaveChanges();
+            return View("Login");
+        }
+
+        public ActionResult Logout() { 
+
+            Session.Clear();
+            Session.Abandon();
+
+            ViewBag.Logout = "Logged out from the system ! ";
+            
+            return View("Login");
+        }
+
+
+        //Clear login
+        public ViewResult ClearLogin () {
+
+            ModelState.Clear();
+            return View("Login"); 
+        }
+        public ViewResult ClearSignUp()
+        {
+
+            ModelState.Clear();
+            return View("SignUp");
+        }
+
+
+
+    }
+
+
+}
